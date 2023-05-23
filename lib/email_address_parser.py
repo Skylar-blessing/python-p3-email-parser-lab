0fc4d5695664c21b09225946fd90f017786e0eab
Skylar <skylarblessings5@gmail.com>
2023-05-23 03:45:53 +0300

    task

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        addresses = re.split(r'\s|,', self.email_addresses)  # Split on spaces or commas
+        addresses = [address.strip() for address in addresses if address.strip()]  # Remove empty strings
+        addresses = list(set(addresses))  # Remove duplicates
+        addresses.sort()  # Sort alphabetically
+        return addresses
